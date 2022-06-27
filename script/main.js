ebc32ec01c4a88fd4c27c31ea5f5188c37d99d95
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-06-27 16:53:52 -0400

    added animation in - out toggle in burger menu

--- script/main.js
+++ script/main.js
@@ -1,12 +1,23 @@
-const burger = document.querySelector('.burger')
-const nav = document.querySelector('.nav-links')
 
-
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
 
 const navSlide = () => {
+   //burger menu clicked, toggles class nav-active to animate in
   burger.addEventListener('click',() => {
-  nav.classList.toggle('nav-active');
+    nav.classList.toggle('nav-active');
+    
+    //animate in li's
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        
+          link.style.animation =''; //reset the animation in
       
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`; //timer for animation in
+        }
+    });
   });
 
 }
