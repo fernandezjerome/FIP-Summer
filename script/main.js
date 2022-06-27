d57a506aeeffc20d7c1c5442b535d1ee9c0b2e0a
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-06-27 17:52:02 -0400

    finished animate in out on navigation to mobile and tablet

--- script/main.js
+++ script/main.js
@@ -10,16 +10,15 @@ const navSlide = () => {
     
     //animate in li's
     navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        
-          link.style.animation =''; //reset the animation in
-      
+      if (link.style.animation) {       
+          link.style.animation =''; //reset the animation in     
         } else {
           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`; //timer for animation in
         }
     });
+    //burger animation - add toggle classlist to burger to activite the animation
+    burger.classList.toggle('toggle');
   });
-
 }
 
 navSlide();
