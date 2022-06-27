da6ccc4ebef2387f01369db7f3148c2867a7f0ed
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-06-27 15:42:23 -0400

    adding mobile nav

--- script/main.js
+++ script/main.js
@@ -1,9 +1,12 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger')
-  const nav = document.querySelector('.nav-links')
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.nav-links')
 
+
+
+const navSlide = () => {
   burger.addEventListener('click',() => {
-      nav.classList.toggle('nav-active');
+  nav.classList.toggle('nav-active');
+      
   });
 
 }
